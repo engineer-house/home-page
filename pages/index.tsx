69e9d4e5c0c70e6c0eb881f29a7bc69a95887298
teto <48591301+teto-soft@users.noreply.github.com>
2022-09-27 01:49:36 +0900

    hided logo

--- pages/index.tsx
+++ pages/index.tsx
@@ -30,13 +30,13 @@ const Home: NextPage = () => (
       </Heading>
     </VStack>
 
-    <Image
-      mx="auto"
-      borderRadius="full"
-      boxSize="xs"
-      src="/engineer_house_logo.png"
-      alt="engineer_house_logo"
-    />
+    {/*<Image*/}
+    {/*  mx="auto"*/}
+    {/*  borderRadius="full"*/}
+    {/*  boxSize="xs"*/}
+    {/*  src="/engineer_house_logo.png"*/}
+    {/*  alt="engineer_house_logo"*/}
+    {/*/>*/}
 
     <EhCard title="ABOUT">
       <Text m="0" fontSize="lg">
